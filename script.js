92fbe1e28dd57a6382cdb95d61224bdf28285734
Phahose <ekwomnick@gmail.com>
2023-03-20 18:23:01 -0600

    The sticky header added

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const swup = new Swup();
+const header = document.getElementById('header');
+window.onscroll = function(){scrollFunction()};
+function scrollFunction(){
+    if (window.scrollY > 50 || window.pageYOffset > 50) {
+        header.classList.add("header-bar")
+      }
+      else{
+        header.classList.remove("header-bar")
+      }
+}
